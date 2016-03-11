fd876ba5a20244ad862ebc37446bf6487b01e7b4
Christopher Adams <christopher@fabricatorz.com>
2016-03-11 17:01:37 +0800

    List workspaces on xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -11,6 +11,14 @@ main = do
 xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
 xmonad $ defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
+    , logHook = dynamicLogWithPP $ xmobarPP
+        { ppOutput = hPutStrLn xmproc
+        , ppCurrent = xmobarColor "#6A9FB5" "" . wrap "[" "]"
+        , ppHidden = xmobarColor "#505050" ""
+        , ppHiddenNoWindows = xmobarColor "#AAAAAA" ""
+        , ppUrgent = xmobarColor "#CCCCCC" ""
+        , ppTitle = xmobarColor "#505050" "" . shorten 7
+        }
     , layoutHook = smartBorders $ avoidStruts  $  layoutHook defaultConfig
 	, borderWidth = 1
 	, focusedBorderColor = "#AC4142"
