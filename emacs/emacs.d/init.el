53da5060704347cfa3331d41e5dd813c3f85e11d
Christopher Adams <christopher@fabricatorz.com>
2016-03-15 17:49:48 +0800

    Get web-mode working for html files

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -151,7 +151,7 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-content-types-alist
 		  '(("jsx" . "\\.js[x]?\\'")))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.htm[l]?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.[s]?css\\'" . web-mode)))
 
