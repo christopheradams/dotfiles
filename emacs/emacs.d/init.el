731902b72375d60dcb02dc2ab19d120fb9f52728
Christopher Adams <christopher@fabricatorz.com>
2016-06-10 12:05:03 +0800

    Don't add note when changing to STARTED state
    But keep the timestamp

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -104,7 +104,7 @@
 (define-key global-map "\C-cc" 'org-capture)
 
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "IDEA(i)" "NEXT(n)" "STARTED(s@/!)" "WAITING(w@/!)" "|" "DONE(d)")
+      (quote ((sequence "TODO(t)" "IDEA(i)" "NEXT(n)" "STARTED(s!)" "WAITING(w@/!)" "|" "DONE(d)")
               (sequence "|" "CANCELLED(c@/!)" "DELEGATED(l@/!)" "DEFERRED(f!)"))))
 
 (setq org-todo-keyword-faces
