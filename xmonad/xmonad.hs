88140e8e7a1ba5bc4c7e58498dee4aa38270e7d8
Christopher Adams <christopher@fabricatorz.com>
2016-11-01 20:02:58 +0800

    Set compose key to ralt

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -9,6 +9,7 @@ import System.IO
 
 main = do
 spawn "dropbox start"
+spawn "setxkbmap -option 'compose:ralt'"
 xmproc <- spawnPipe "xmobar ~/.xmobarrc"
 xmonad $ defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
