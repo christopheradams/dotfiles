d7a565550f48f55e00f72f7bf7d4d820f610e619
Christopher Adams <christopher@fabricatorz.com>
2017-05-28 12:50:50 +0800

    Put custom-set-variables in their own file

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -2,29 +2,11 @@
 (server-start)
 
 ;;(setq gc-cons-threshold (* 10 1024 1024))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#101010" "#7c7c7c" "#8e8e8e" "#a0a0a0" "#686868" "#747474" "#686868" "#b9b9b9"])
- '(ansi-term-color-vector
-   [unspecified "#101010" "#7c7c7c" "#8e8e8e" "#a0a0a0" "#686868" "#747474" "#686868" "#b9b9b9"])
- '(custom-enabled-themes (quote (base16-grayscale-light)))
- '(custom-safe-themes (quote (default)))
- '(package-selected-packages
-   (quote
-    (hlinum yaml-mode web-mode use-package spaceline smex slim-mode ruby-end rainbow-mode projectile php-mode markdown-mode magit init-open-recentf ibuffer-vc haskell-mode git-gutter-fringe format-sql exec-path-from-shell evil-leader eproject edts editorconfig diff-hl company-restclient column-marker auctex alchemist ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-date-weekend ((t (:inherit org-agenda-date :weight normal :slant italic))) t)
- '(org-document-title ((t (:foreground "#999999" :weight bold :height 1.0)))))
+
+;; custom set-variables and set-faces
+(let ((custom.el (expand-file-name "custom.el" user-emacs-directory)))
+  (setq custom-file custom.el)
+  (load custom.el))
 
 ;;; Themes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
