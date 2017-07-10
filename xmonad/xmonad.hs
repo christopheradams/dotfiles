66b2751a1cc2b7a978ab26bcbdfcc11eb4521cd0
Christopher Adams <christopher@fabricatorz.com>
2017-07-10 10:38:13 +0800

    Start dropbox

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -16,6 +16,7 @@ myLayout = ( smartBorders $ avoidStruts  (resizableTile ||| Mirror resizableTile
     delta = 3/100
 
 main = do
+xmproc <- spawnPipe "dropbox start"
 xmproc <- spawnPipe "xmobar ~/.xmobarrc"
 xmonad $ docks defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
