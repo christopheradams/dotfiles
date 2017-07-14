1fc0bedc49c748484f596b9ffe42f2209cdbe8e4
Christopher Adams <christopher@fabricatorz.com>
2017-07-14 10:35:56 +0800

    Update custom variables

--- emacs/emacs.d/custom.el
+++ emacs/emacs.d/custom.el
@@ -8,12 +8,16 @@
  '(ansi-color-names-vector
    ["#101010" "#7c7c7c" "#8e8e8e" "#a0a0a0" "#686868" "#747474" "#686868" "#b9b9b9"])
  '(ansi-term-color-vector
-   [unspecified "#101010" "#7c7c7c" "#8e8e8e" "#a0a0a0" "#686868" "#747474" "#686868" "#b9b9b9"])
+   [unspecified "#101010" "#7c7c7c" "#8e8e8e" "#a0a0a0" "#686868" "#747474" "#686868" "#b9b9b9"] t)
+ '(column-number-mode t)
  '(custom-enabled-themes (quote (base16-grayscale-light)))
  '(custom-safe-themes (quote (default)))
+ '(font-use-system-font t)
  '(package-selected-packages
    (quote
-    (ivy hlinum yaml-mode web-mode use-package spaceline smex slim-mode ruby-end rainbow-mode projectile php-mode markdown-mode magit init-open-recentf ibuffer-vc haskell-mode git-gutter-fringe format-sql exec-path-from-shell evil-leader eproject edts editorconfig diff-hl company-restclient column-marker auctex alchemist ag))))
+    (counsel-projectile ivy hlinum yaml-mode web-mode use-package spaceline smex slim-mode ruby-end rainbow-mode projectile php-mode markdown-mode magit init-open-recentf ibuffer-vc haskell-mode git-gutter-fringe format-sql exec-path-from-shell evil-leader eproject edts editorconfig diff-hl company-restclient column-marker auctex alchemist ag)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
