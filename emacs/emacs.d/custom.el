c180cd2269c2103381238fe134ed3d8f679d5122
Christopher Adams <christopher@fabricatorz.com>
2017-09-10 14:50:02 +0800

    Load base16-theme

--- emacs/emacs.d/custom.el
+++ emacs/emacs.d/custom.el
@@ -13,12 +13,41 @@
  '(custom-enabled-themes (quote (base16-grayscale-light)))
  '(custom-safe-themes (quote (default)))
  '(debug-on-error nil)
+ '(fci-rule-color "#383a42")
  '(font-use-system-font t)
+ '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#4078f2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#50a14f"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#9ca0a4"))
+ '(org-fontify-done-headline t)
+ '(org-fontify-quote-and-verse-blocks t)
+ '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-    (epresent org-clock-convenience gscholar-bibtex org-plus-contrib counsel-projectile ivy hlinum yaml-mode web-mode use-package spaceline smex slim-mode ruby-end rainbow-mode projectile php-mode markdown-mode magit init-open-recentf ibuffer-vc haskell-mode git-gutter-fringe format-sql exec-path-from-shell evil-leader eproject edts editorconfig diff-hl company-restclient column-marker auctex alchemist ag)))
+    (base16-theme epresent org-clock-convenience gscholar-bibtex org-plus-contrib counsel-projectile ivy hlinum yaml-mode web-mode use-package spaceline smex slim-mode ruby-end rainbow-mode projectile php-mode markdown-mode magit init-open-recentf ibuffer-vc haskell-mode git-gutter-fringe format-sql exec-path-from-shell evil-leader eproject edts editorconfig diff-hl company-restclient column-marker auctex alchemist ag)))
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#f0f0f0")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50a14f")
+    (cons 40 "#688e35")
+    (cons 60 "#807b1b")
+    (cons 80 "#986801")
+    (cons 100 "#ae7118")
+    (cons 120 "#c37b30")
+    (cons 140 "#da8548")
+    (cons 160 "#c86566")
+    (cons 180 "#b74585")
+    (cons 200 "#a626a4")
+    (cons 220 "#ba3685")
+    (cons 240 "#cf4667")
+    (cons 260 "#e45649")
+    (cons 280 "#d2685f")
+    (cons 300 "#c07b76")
+    (cons 320 "#ae8d8d")
+    (cons 340 "#383a42")
+    (cons 360 "#383a42")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
