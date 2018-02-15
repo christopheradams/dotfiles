3c29d97e3003308d75512b3daaf143660dcb4175
Christopher Adams <christopher@fabricatorz.com>
2018-02-15 10:47:05 +0800

    Launch chrome instead of chromium

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -37,7 +37,7 @@ xmonad $ docks defaultConfig
     [ ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock") --mod4mask is the windows key
     , ((mod1Mask .|. shiftMask, xK_e), spawnHere "emacsclient --no-wait --create-frame --alternate-editor='' --eval '(switch-to-buffer nil)'") -- %! Launch emacs
     , ((mod1Mask .|. shiftMask, xK_f), spawnHere "firefox") -- %! Launch Firefox
-    , ((mod1Mask .|. shiftMask, xK_g), spawnHere "chromium-browser") -- %! Launch Chromium
+    , ((mod1Mask .|. shiftMask, xK_g), spawnHere "google-chrome") -- %! Launch Chrome
     , ((mod1Mask .|. shiftMask, xK_r), spawnHere "nautilus -w") -- %! Launch Nautilus
     , ((mod1Mask .|. shiftMask, xK_t), spawnHere "thunderbird") -- %! Launch Thunderbird
     , ((mod1Mask .|. shiftMask, xK_u), spawnHere "unity-control-center network")
