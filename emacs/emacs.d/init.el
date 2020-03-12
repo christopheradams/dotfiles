9ea4093295e038dfc1429ed0ddeb3b6fa0dc0bf3
Christopher Adams <christopher@fabricatorz.com>
2020-03-12 11:26:46 +0800

    Add fill column copy region command

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -406,6 +406,23 @@
   (evil-set-initial-state 'epresent-mode 'emacs)
   (evil-set-initial-state 'dired-mode 'emacs))
 
+; https://gist.github.com/xahlee/d364cbbff9b3abd12d29
+(defun cxa-copy-simple (&optional beg end)
+  "Save the current region (or line) to the `kill-ring' after stripping extra whitespace and new lines"
+  (interactive
+   (if (region-active-p)
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-end-position))))
+  (let ((my-text (buffer-substring-no-properties beg end)))
+    (with-temp-buffer
+      (insert my-text)
+      (goto-char 1)
+      (while (looking-at "[ \t\n]")
+        (delete-char 1))
+      (let ((fill-column 9333999))
+        (fill-region (point-min) (point-max)))
+      (kill-region (point-min) (point-max)))))
+
 ;;; Smex
 (use-package smex
   :ensure t
