01d45d7706f25a552de50e341ceeee31e83caf2d
Christopher Adams <christopher@cxadams.com>
2020-06-12 15:45:58 +0800

    Don't keep point when splitting window below
    Keeps the text on screen close to what it was before the split, and if
    the cursor is in the bottom half on the window, select bottom split.

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -64,6 +64,9 @@
 ;;; Frames
 (add-hook 'after-make-frame-functions 'cxa-contextual-menubar)
 
+;;; Windows
+(setq split-window-keep-point nil)
+
 ;;; Window Resize
 (global-set-key (kbd "C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "C-<right>") 'enlarge-window-horizontally)
