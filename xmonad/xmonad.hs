876118303e5cc091ae07a84dd2545e039a894708
Christopher Adams <christopher@cxadams.com>
2025-02-01 13:20:09 +0800

    Sort scratchpads by name

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -24,12 +24,12 @@ import System.IO
 scratchpads :: [NamedScratchpad]
 scratchpads =
   [ NS "1password" "1password" (className =? "1Password") doCenterFloat
-  , NS "gnome-clocks" "gnome-clocks" (className =? "Org.gnome.clocks") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
-  , NS "gnome-control-center" "gnome-control-center network" (className =? "Gnome-control-center") doCenterFloat
-  , NS "gnome-system-monitor" "gnome-system-monitor" (className =? "Gnome-system-monitor") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
-  , NS "terminal" "gnome-terminal --class Gnome-terminal-scratch" (className =? "Gnome-terminal-scratch") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
-  , NS "qalculate" "qalculate" (className =? "Qalculate-gtk") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
+  , NS "gnome-clocks" "gnome-clocks" (className =? "org.gnome.clocks") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
+  , NS "gnome-control-center" "gnome-control-center network" (className =? "gnome-control-center") doCenterFloat
+  , NS "gnome-system-monitor" "gnome-system-monitor" (className =? "gnome-system-monitor") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
   , NS "nextcloud" "nextcloud-desktop-client.nextcloud" (className =? "Nextcloud") doCenterFloat
+  , NS "qalculate" "qalculate" (className =? "Qalculate-gtk") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
+  , NS "terminal" "gnome-terminal --class Gnome-terminal-scratch" (className =? "Gnome-terminal-scratch") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
   ]
 
 myLayout = ( smartBorders $ avoidStruts  (resizableTile ||| threeColMid ||| mirrorResizableTile ||| Full ))
