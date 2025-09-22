2483f45a556353e5ea61f0557495d8f1171ead04
Christopher Adams <christopher@cxadams.com>
2025-09-22 20:31:52 +0800

    Add text cpu, memory, network to taffybar

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -2,6 +2,9 @@ module Main where
 
 import System.Taffybar.SimpleConfig
 import System.Taffybar.Widget.SimpleClock
+import System.Taffybar.Widget.Text.CPUMonitor
+import System.Taffybar.Widget.Text.MemoryMonitor
+import System.Taffybar.Widget.Text.NetworkMonitor
 import System.Taffybar.Widget.Workspaces
 
 myClockConfig :: ClockConfig
@@ -10,7 +13,11 @@ myClockConfig = defaultClockConfig
 
 main :: IO ()
 main = simpleTaffybar defaultSimpleTaffyConfig
-  { startWidgets = [workspacesNew defaultWorkspacesConfig]
-  , endWidgets = [textClockNewWith myClockConfig]
+       { startWidgets = [workspacesNew defaultWorkspacesConfig
+                        , textCpuMonitorNew "| $total$%" 1.0
+                        , textMemoryMonitorNew "| $used$" 1.0
+                        , networkMonitorNew "| ▼ $inAuto$ ▲ $outAuto$" Nothing
+                        ]
+  , endWidgets = [ textClockNewWith myClockConfig]
   , barHeight    = ScreenRatio (1 / 40)  -- smaller than default 1/27
   }
