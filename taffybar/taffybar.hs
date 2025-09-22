9825e7201a402edc4efdcab44012842b565eb8cc
Christopher Adams <christopher@cxadams.com>
2025-09-22 19:38:09 +0800

    Make taffybar show a 24 hour clock

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -4,9 +4,13 @@ import System.Taffybar.SimpleConfig
 import System.Taffybar.Widget.SimpleClock
 import System.Taffybar.Widget.Workspaces
 
+myClockConfig :: ClockConfig
+myClockConfig = defaultClockConfig
+  { clockFormatString = "%a %b %d %Y %H:%M" } -- 24 hour clock
+
 main :: IO ()
 main = simpleTaffybar defaultSimpleTaffyConfig
   { startWidgets = [workspacesNew defaultWorkspacesConfig]
-  , endWidgets = [textClockNewWith defaultClockConfig]
+  , endWidgets = [textClockNewWith myClockConfig]
   , barHeight    = ScreenRatio (1 / 40)  -- smaller than default 1/27
   }
