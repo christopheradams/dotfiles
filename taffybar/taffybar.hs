3be0e20a884a8b70a6938ed8f14ed7d94ab58030
Christopher Adams <christopher@cxadams.com>
2025-09-22 20:43:23 +0800

    Add UTC time to taffybar

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -7,9 +7,15 @@ import System.Taffybar.Widget.Text.MemoryMonitor
 import System.Taffybar.Widget.Text.NetworkMonitor
 import System.Taffybar.Widget.Workspaces
 
+import Data.Time.LocalTime (utc)
+
 myClockConfig :: ClockConfig
 myClockConfig = defaultClockConfig
   { clockFormatString = "%a %b %d %Y %H:%M %Z" } -- 24 hour clock
+
+utcClockConfig :: ClockConfig
+utcClockConfig = defaultClockConfig
+  { clockTimeZone = Just utc, clockFormatString = "%H:%M %Z |" }
 
 main :: IO ()
 main = simpleTaffybar defaultSimpleTaffyConfig
@@ -18,6 +24,8 @@ main = simpleTaffybar defaultSimpleTaffyConfig
                         , textMemoryMonitorNew "| $used$" 1.0
                         , networkMonitorNew "| ▼ $inAuto$ ▲ $outAuto$" Nothing
                         ]
-  , endWidgets = [ textClockNewWith myClockConfig]
+  , endWidgets = [ textClockNewWith myClockConfig
+                 , textClockNewWith utcClockConfig
+                 ]
   , barHeight    = ScreenRatio (1 / 40)  -- smaller than default 1/27
   }
