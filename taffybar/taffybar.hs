ee7c707cef68a2f3c9f01d3d03ed7c542a638803
Christopher Adams <christopher@cxadams.com>
2025-09-22 18:58:25 +0800

    Update taffybar config and style

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -2,10 +2,37 @@ module Main where
 
 import System.Taffybar.SimpleConfig
 import System.Taffybar.Widget.SimpleClock
+import System.Taffybar.Widget.Text.CPUMonitor
+import System.Taffybar.Widget.Text.MemoryMonitor
+import System.Taffybar.Widget.Text.NetworkMonitor
 import System.Taffybar.Widget.Workspaces
+
+import Data.Time.LocalTime (utc)
+
+myWorkspacesConfig :: WorkspacesConfig
+myWorkspacesConfig = defaultWorkspacesConfig
+  { showWorkspaceFn = hideEmpty -- hide empty workspaces
+  -- , maxIcons = Just 0 -- hide icons
+  }
+
+myClockConfig :: ClockConfig
+myClockConfig = defaultClockConfig
+  { clockFormatString = "%a %b %d %Y %H:%M %Z" } -- 24 hour clock
+
+utcClockConfig :: ClockConfig
+utcClockConfig = defaultClockConfig
+  { clockTimeZone = Just utc, clockFormatString = "%H:%M %Z |" }
 
 main :: IO ()
 main = simpleTaffybar defaultSimpleTaffyConfig
-  { startWidgets = [workspacesNew defaultWorkspacesConfig]
-  , endWidgets = [textClockNewWith defaultClockConfig]
-  }
+       { startWidgets = [workspacesNew myWorkspacesConfig
+                        , textCpuMonitorNew "| $total$%" 1.0
+                        , textMemoryMonitorNew "| $used$" 1.0
+                        , networkMonitorNew "| ▼ $inAuto$ ▲ $outAuto$" Nothing
+                        ]
+       , endWidgets = [ textClockNewWith myClockConfig
+                      , textClockNewWith utcClockConfig
+                      ]
+       , barHeight    = ExactSize 18  -- default is ScreenRatio (1 / 27)
+       , barPosition  = Bottom
+    }
