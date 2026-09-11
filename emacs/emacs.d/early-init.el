432f7fdd7c67f4196c4b72705ce6d8a7c1a18f3b
Christopher Adams <christopher@cxadams.com>
2026-09-11 16:00:35 +0800

    Stop warnings for missing lexical binding cookie

--- emacs/emacs.d/early-init.el
+++ emacs/emacs.d/early-init.el
@@ -3,5 +3,8 @@
 ;; prevent package.el loading packages prior to their init-file loading
 (setq package-enable-at-startup nil)
 
+;; Warnings
+(setq warning-inhibit-types '((files missing-lexbind-cookie)))
+
 ;; disable native compilation
 ;; (setq native-comp-speed -1)
